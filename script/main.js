bf72e7ccc024a09614076ab790f803ca51217caa
mingsong01 <107245677+mingsong01@users.noreply.github.com>
2024-12-08 23:06:55 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -167,47 +167,47 @@ const animationTimeline = () => {
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
-      scale: 1.2,
-      x: 10,
-      backgroundColor: "rgb(21, 161, 237)",
-      color: "#fff"
-    })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(
-      ".idea-5",
-      0.7,
-      {
-        rotationX: 15,
-        rotationZ: -10,
-        skewY: "-5deg",
-        y: 50,
-        z: 10,
-        opacity: 0
-      },
-      "+=0.5"
-    )
-    .to(
-      ".idea-5 .smiley",
-      0.7,
-      {
-        rotation: 90,
-        x: 8
-      },
-      "+=0.4"
-    )
-    .to(
-      ".idea-5",
-      0.7,
-      {
-        scale: 0.2,
-        opacity: 0
-      },
-      "+=2"
-    )
+    // .from(".idea-3", 0.7, ideaTextTrans)
+    // .to(".idea-3 strong", 0.5, {
+    //   scale: 1.2,
+    //   x: 10,
+    //   backgroundColor: "rgb(21, 161, 237)",
+    //   color: "#fff"
+    // })
+    // .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    // .from(".idea-4", 0.7, ideaTextTrans)
+    // .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    // .from(
+    //   ".idea-5",
+    //   0.7,
+    //   {
+    //     rotationX: 15,
+    //     rotationZ: -10,
+    //     skewY: "-5deg",
+    //     y: 50,
+    //     z: 10,
+    //     opacity: 0
+    //   },
+    //   "+=0.5"
+    // )
+    // .to(
+    //   ".idea-5 .smiley",
+    //   0.7,
+    //   {
+    //     rotation: 90,
+    //     x: 8
+    //   },
+    //   "+=0.4"
+    // )
+    // .to(
+    //   ".idea-5",
+    //   0.7,
+    //   {
+    //     scale: 0.2,
+    //     opacity: 0
+    //   },
+    //   "+=2"
+    // )
     .staggerFrom(
       ".idea-6 span",
       0.8,
